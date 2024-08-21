1decd9d2383c3ebacb8579847f52e47e90fc4b53
sarbas33 <sarbas33@gmail.com>
2024-08-21 23:11:08 +0530

    Update apiService endpoint to beeceptor

--- src/services/apiServices.tsx
+++ src/services/apiServices.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchDataFromApi = async () => {
   try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const response = await axios.get('https://erpcollege.free.beeceptor.com/data');
     return response.data;
   } catch (error) {
     console.error('API call failed:', error);
