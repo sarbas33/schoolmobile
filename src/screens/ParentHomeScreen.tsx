1ffcc46761d1648aa783cba1f5b23742c36d12a5
sarbas33 <sarbas33@gmail.com>
2024-08-27 01:27:12 +0530

    Add icons to navigation bar, Implement ProfileScreen

--- src/screens/ParentHomeScreen.tsx
+++ src/screens/ParentHomeScreen.tsx
@@ -1,23 +1,54 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import HomeScreen from './parent/HomeScreen';
 import AcademicsParentNavigator from './AcademicsParentNavigator';
 import FeesParentNavigator from './FeesParentNavigator';
 import AttendanceScreen from './parent/AttendanceScreen';
-import TransportationScreen from './parent/TransportationScreen';
+import ProfileScreen from './parent/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
 const ParentHomeScreen = () => {
   return (
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Home"  component={HomeScreen} />
-        <Tab.Screen name="Academics" component={AcademicsParentNavigator} />
-        <Tab.Screen name="Fees" component={FeesParentNavigator} />
-        <Tab.Screen name="Transportation" component={TransportationScreen} />
-      </Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color = 'gray', size = 24 }) => {
+          let iconName;
 
+          // Customize the icon based on the route name
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Academics') {
+            iconName = focused ? 'book' : 'book-outline';
+          } else if (route.name === 'Fees') {
+            iconName = focused ? 'card' : 'card-outline';
+          } else if (route.name === 'Attendance') {
+            iconName = focused ? 'checkmark-circle' : 'checkmark-circle-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+
+          // Return the icon component
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'black', // Active tab color
+        tabBarInactiveTintColor: 'gray', // Inactive tab color
+        tabBarStyle: {
+          backgroundColor: '#fff', // Background color of the tab bar
+          height: 60, // Height of the tab bar
+        },
+        tabBarLabelStyle: {
+          fontSize: 12, // Label font size
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Academics" component={AcademicsParentNavigator} />
+      <Tab.Screen name="Fees" component={FeesParentNavigator} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
   );
 };
 
